89820fdeb70e8fb63c2ead4d60c351423a4a9979
Bekbolot Bekmamatov <bbekmamatov1991@gmail.com>
2022-01-10 16:06:12 +0600

    init

---
+++ book/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from . import models
+
+def book_all(request):
+    post = models.Posts.objects.all()
+    return render(request, "post_list.html", {'post': post})
