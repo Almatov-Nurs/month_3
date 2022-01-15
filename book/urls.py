1afc233d2ca6c4a2cce002628f28bbd98e7cc7de
Bekbolot Bekmamatov <bbekmamatov1991@gmail.com>
2022-01-16 03:08:52 +0600

    ohuubiu

--- book/urls.py
+++ book/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('book/',views.book_all, name='book_all'),
-    path('book/<int:id>/',views.book_detail, name='book_detail')
+    path('book/<int:id>/',views.book_detail, name='book_detail'),
+    path('add-book/',views.add_book, name='add-book')
 ]
--- book/views.py
+++ book/views.py
@@ -1,10 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 def book_all(request):
     post = models.Book.objects.all()
-    return render(request, "post_list.html", {'post': post})
+    return render(request, "book_list.html", {'post': post})
 
 def book_detail(request, id):
     book = get_object_or_404(models.Book, id=id)
     return render(request, 'book_detail.html', {'book': book})
+
+def add_book(request):
+    method = request.method
+    if method == "POST":
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Show created')
+    else:
+        form = forms.BookForm()
+    return render(request, "add_book.html", {"form": form})
